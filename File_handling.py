8d3a85bb427395a9c4ae8149dd03b73077779038
aminboyev01 <aminboyevshohrux76@gmail.com>
2025-10-02 14:26:10 +0500

    ozgardi

--- File_handling.py
+++ File_handling.py
@@ -2,23 +2,19 @@ import pickle
 
 faylname = "C:/pi_million_digits.txt"
 
-
-with open(faylname,"r") as file:
+with open(faylname, "r") as file:
     file_read = file.read()
     if "30012006" in file_read:
         print("topildi")
     else:
         print("topilmadi")
 
+pi_str = file_read.strip().replace("\n", "")
 
-pi_float = float(file_read)
+with open("pickle_file.pkl", "wb") as f:
+    pickle.dump(pi_str, f)
 
-
-with open("pickle_file","wb") as f:
-    pickle.dump(pi_float, f)
-
-
-with open("pickle_file","rb") as f:
+with open("pickle_file.pkl", "rb") as f:
     ss = pickle.load(f)
 
 print("Pickledan o‘qildi:", ss)
