776a7a93715e779b52ea28d524a0ed515ddcb00b
aminboyev01 <aminboyevshohrux76@gmail.com>
2025-11-22 03:26:26 +0500

    qayta_joylandi
    telegram bot repoda

--- bot_sqllite/send_buttons/send_menu.py
+++
@@ -1,8 +0,0 @@
-from telegram import ReplyKeyboardMarkup
-
-def main_menu():
-    markup = ReplyKeyboardMarkup(
-        [['Country', 'Job']],
-        resize_keyboard=True
-    )
-    return markup
